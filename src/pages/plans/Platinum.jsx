3faa602d358156b355fb795d98eff14124dd801f
K Mordhwaj <kmordhwaz@gmail.com>
2022-10-28 22:15:16 +0530

    some comp seo updated

--- src/pages/plans/Platinum.jsx
+++ src/pages/plans/Platinum.jsx
@@ -2,22 +2,41 @@ import React from "react";
 import Footer from "../../components/footer/Footer";
 import Navbar from "../../components/navbar/Navbar";
 import "./plans.css";
+import { Helmet } from "react-helmet";
 
-const Platinum = () => {
+const Platinum = ({ title }) => {
   return (
-    <div className="home">
-      <Navbar />
-      <div className="homeContainer">
-        <div className="memPlans">
-          <h1>Weekend Gateways</h1>
-          <div className="memTexts">
-            Planning for a Weekend Gateway? Contact us!
+    <>
+      {/* seo portion */}
+      <Helmet>
+        <meta charset="utf-8" />
+        <title>{title}</title>
+        <meta name="description" content="Platinum plan for the trip." />
+        <meta
+          name="keywords"
+          content="Swarn-holidays swarn-holidays toor travel holiday-packages swarn-holidays-raipur swarn-holidays-plans swarn-holidays-platinum-plan"
+        />
+      </Helmet>
+
+      {/* code */}
+      <div className="home">
+        <Navbar />
+        <div className="homeContainer">
+          <div className="memPlans">
+            <h1>Weekend Gateways</h1>
+            <div className="memTexts">
+              Planning for a Weekend Gateway? Contact us!
+            </div>
           </div>
+          <Footer />
         </div>
-        <Footer />
       </div>
-    </div>
+    </>
   );
+};
+
+Platinum.defaultProps = {
+  title: "Swarn Holidays | Plans - Platinum",
 };
 
 export default Platinum;
--- src/pages/plans/Silver.jsx
+++ src/pages/plans/Silver.jsx
@@ -2,22 +2,41 @@ import React from "react";
 import "./plans.css";
 import Footer from "../../components/footer/Footer";
 import Navbar from "../../components/navbar/Navbar";
+import { Helmet } from "react-helmet";
 
-const Silver = () => {
+const Silver = ({ title }) => {
   return (
-    <div className="home">
-      <Navbar />
-      <div className="homeContainer">
-        <div className="memPlans">
-          <h1>Long Holidays</h1>
-          <div className="memTexts">
-            Planning for a long holiday with group? Contact us!
+    <>
+      {/* seo portion */}
+      <Helmet>
+        <meta charset="utf-8" />
+        <title>{title}</title>
+        <meta name="description" content="Silver plan for the trip." />
+        <meta
+          name="keywords"
+          content="Swarn-holidays swarn-holidays toor travel holiday-packages swarn-holidays-raipur swarn-holidays-plans swarn-holidays-silver-plan"
+        />
+      </Helmet>
+
+      {/* code */}
+      <div className="home">
+        <Navbar />
+        <div className="homeContainer">
+          <div className="memPlans">
+            <h1>Long Holidays</h1>
+            <div className="memTexts">
+              Planning for a long holiday with group? Contact us!
+            </div>
           </div>
+          <Footer />
         </div>
-        <Footer />
       </div>
-    </div>
+    </>
   );
+};
+
+Silver.defaultProps = {
+  title: "Swarn Holidays | Plans - Silver",
 };
 
 export default Silver;
